8d6ccf6509ab7848f930cc60762204ffcc66a4ea
Adarsh <83777943+adarsh-67r@users.noreply.github.com>
2026-07-25 21:59:39 +0530

    Time: 35 ms (98.9%), Space: 29.2 MB (47.49%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -18,7 +18,6 @@ public:
                     l++;
                     r--;
                     while (l < r && nums[l] == nums[l - 1]) l++;
-                    while (l < r && nums[r] == nums[r + 1]) r--;
                 }
             }
         }
