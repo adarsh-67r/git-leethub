8aad532362edafddca16a500f53362a1e222784d
Adarsh <83777943+adarsh-67r@users.noreply.github.com>
2026-07-25 21:57:36 +0530

    Time: 39 ms (94.9%), Space: 29.1 MB (47.49%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -5,6 +5,7 @@ public:
         sort(nums.begin(), nums.end());
         vector<vector<int>> ans;
         for (int i = 0; i < n; i++) {
+            if (nums[i] > 0) break;
             if (i > 0 && nums[i] == nums[i - 1]) continue;
             int l = i + 1;
             int r = n - 1;
